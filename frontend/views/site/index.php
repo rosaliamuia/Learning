8c8ec57646c76138aa1303fe0905fbd3ec507627
Rosalia Muia <rosaliakaluki@gmail.com>
2020-09-23 10:14:40 +0300

    first commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+use yii\helpers\Url;
+
+
+/* @var $this yii\web\View */
+
+$this->title = 'Rosalia Portfolio';
+?>
+      <h1>Rosalia Kaluki</h1>
+      <h2>I'm a professional Web Developer from Machakos, Kenya</h2>
+      <a href="<?= Url::to(['site/about'])?>" class="btn-about">About Me</a>
